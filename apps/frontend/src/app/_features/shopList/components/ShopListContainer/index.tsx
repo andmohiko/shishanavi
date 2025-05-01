65f4739aa6ad2929eec22c905776b9abf5360d52
y.miyake <yutomiyake106412@gmail.com>
2025-05-01 14:02:45 +0900

    searchタグ作成

--- apps/frontend/src/app/_features/shopList/components/ShopListContainer/index.tsx
+++ apps/frontend/src/app/_features/shopList/components/ShopListContainer/index.tsx
@@ -4,14 +4,20 @@ import styles from './styles.module.css'
 import type { Card } from './type'
 
 import { CardList } from '~/app/_features/shopList/components/CardList'
+import { SearchAreaTag } from '~/app/_features/shopList/components/SearchAreaTag'
 
 type Props = {
   shopList: Array<Card>
+  searchAreas: Array<{ id: number; name: string; isSelected: boolean }>
 }
 
-export const ShopListContainer = ({ shopList }: Props): ReactNode => {
+export const ShopListContainer = ({
+  shopList,
+  searchAreas,
+}: Props): ReactNode => {
   return (
     <div className={styles.shopListContainer}>
+      <SearchAreaTag searchAreas={searchAreas} />
       <CardList shopList={shopList} />
     </div>
   )
--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -8,8 +8,25 @@ export const metadata: Metadata = {
   description: '',
 }
 
-const Page = () => {
-  return <ShopListContainer shopList={cardMock} />
+type Props = {
+  searchParams: Promise<{ area?: string }>
+}
+
+const Page = async ({ searchParams }: Props) => {
+  const { area } = await searchParams
+  const areaId = area ? Number(area) : 1
+  // areaMasterから取得したエリア情報を使用する
+  const searchAreas = [
+    { id: 1, name: '全て' },
+    { id: 2, name: '池袋' },
+    { id: 3, name: '渋谷' },
+    { id: 4, name: '難波' },
+  ].map((area) => ({
+    ...area,
+    isSelected: area.id === areaId,
+  }))
+
+  return <ShopListContainer shopList={cardMock} searchAreas={searchAreas} />
 }
 
 export default Page
