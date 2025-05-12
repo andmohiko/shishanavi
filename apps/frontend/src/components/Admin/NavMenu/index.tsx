3d9328c3c68271752cd380d29d11ec39767711a8
y.miyake <yutomiyake106412@gmail.com>
2025-05-12 20:43:49 +0900

    修正

--- apps/frontend/src/components/Admin/NavMenu/index.tsx
+++ apps/frontend/src/components/Admin/NavMenu/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 import type { ReactNode } from 'react'
-import { usePathname, useSearchParams } from 'next/navigation'
+import { useSearchParams } from 'next/navigation'
 
 import styles from './styles.module.css'
 import { locationNavItems } from './type'
@@ -8,22 +8,19 @@ import { locationNavItems } from './type'
 import { NavItem } from '~/components/Admin/NavItem'
 
 export const NavMenu = (): ReactNode => {
-  const pathname = usePathname()
   const searchParams = useSearchParams()
-
-  const fullPath = searchParams.toString()
-    ? `${pathname}?${searchParams.toString()}`
-    : pathname
+  const areaParam = searchParams.get('area')
 
   return (
     <div className={styles.menuItems}>
-      {locationNavItems.map((item) => (
-        <NavItem
-          key={item.label}
-          {...item}
-          isCurrent={fullPath === item.href}
-        />
-      ))}
+      {locationNavItems.map((item) => {
+        const url = new URL(item.href, 'http://localhost') //BaseURLは適当でOK
+        const itemArea = url.searchParams.get('area') || null
+        const isCurrent =
+          areaParam === itemArea || (itemArea === null && areaParam === null) //「すべて」を選んだ場合の処理
+
+        return <NavItem key={item.label} {...item} isCurrent={isCurrent} />
+      })}
     </div>
   )
 }
